b21ac63ac467395317099c73d8cf6fd1cde01219
Julian Hinsch <jhinsch799@gmail.com>
2019-01-04 15:49:23 -0500

    reverted to server side rendering, not universal

--- src/react/components/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const App = () => {
-    return (
-        <p>Hello React</p>
-    )
-}
-
-export default App;
--- src/server.js
+++ src/server.js
@@ -1,57 +1,23 @@
-/* eslint no-console: "off" */
-
-import path from 'path';
-import { Server } from 'http';
-import Express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter as Router } from 'react-router-dom';
-import { App } from './components/App';
+require('dotenv').config();
+const path = require('path');
+const Express = require('express');
 
 const app = new Express();
-const server = new Server(app);
 
 // use ejs templates
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/pages'));
 
 // define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, 'static')));
 
-// universal routing and rendering
-app.get('*', (req, res) => {
-    let markup = '';
-    let status = 200;
-
-    const context = {};
-    markup = renderToString(
-      <Router location={req.url} context={context}>
-        <App />
-      </Router>,
-    );
-
-    // context.url will contain the URL to redirect to if a <Redirect> was used
-    if (context.url) {
-        return res.redirect(302, context.url);
-    }
-
-    if (context.is404) {
-        status = 404;
-    }
-
-    return res.status(status).render('index', { markup });
-});
+app.get('/', (req,res) => res.render('landing.ejs'));
+app.get('/login', (req,res) => res.render('login.ejs'));
+app.get('/signup', (req,res) => res.render('signup.ejs'));
 
 // start the server
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
-server.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.info(
-    `
-      Server running on http://localhost:${port} [${env}]
-      Universal rendering: ${process.env.UNIVERSAL ? 'enabled' : 'disabled'}
-    `);
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log('Listening on', port);
 });
